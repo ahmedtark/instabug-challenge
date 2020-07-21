604d9b762eb0367af5214bd8342dcf37f4cbf7ae
ahmedtark <68503835+ahmedtark@users.noreply.github.com>
2020-07-21 12:17:51 +0200

    Delete SetUp.py

--- SetUp.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from selenium import webdriver
-class SetUp(unittest.TestCase):
-    def setUp(self):
-        self.driver=webdriver.chrome()
-    def teardown(self):
-        if (self.driver!= None):
-            self.driver.close
-            self.driver.quit
